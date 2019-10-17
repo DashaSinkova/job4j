aaf7f4627b929ce322154644bad80b7376b7ddc1
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-17 23:56:39 +0300

    Подсчет суммы чётных чисел в диапазоне[#188173]

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+
+public class CounterTest {
+    @Test
+    public void oneToFive(){
+        Counter count = new Counter();
+        assertThat(count.add(1,5),is(6));
+    }
+    @Test
+    public void zeroToZero(){
+        Counter count = new Counter();
+        assertThat(count.add(0,0),is(0));
+    }
+}
