592b702f6380cb47446a456f0df666e48e96f467
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-18 20:11:37 +0300

    5.8. JavaDoc. Code convention[#188176]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,23 +1,36 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculate
+ *
+ * @author Daria Sinkova miss.sinkova-dasha@yanex.ru
+ */
 public class Calculator {
-    public static void add(double first, double second){
-         System.out.println(first+" + " +  second + " = " + (first+second));
-        }
-        public  static  void  main(String args[]){
-        add(1,1);
-        div(4,2);
-        multiply(2,1);
-        subtract(10,5);
-
-        }
-        public static void div(double first, double second){
-        System.out.println(first+" / " + second + " = " +(first/second));
-        }
-    public static void multiply(double first, double second){
-        System.out.println(first+" * " + second + " = " + (first*second));
+    public static void add(double first, double second) {
+        System.out.println(first + " + " + second + " = " + (first + second));
     }
-    public static void subtract(double first, double second){
-        System.out.println(first+" - " + second + " = " +(first-second));
+
+    /**
+     * Main
+     * @param args
+     */
+    public static void main(String[] args) {
+        add(1, 1);
+        div(4, 2);
+        multiply(2, 1);
+        subtract(10, 5);
+
+    }
+
+    public static void div(double first, double second) {
+        System.out.println(first + " / " + second + " = " + (first / second));
+    }
+
+    public static void multiply(double first, double second) {
+        System.out.println(first + " * " + second + " = " + (first * second));
+    }
+
+    public static void subtract(double first, double second) {
+        System.out.println(first + " - " + second + " = " + (first - second));
     }
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *Package for calculator task.
+ *
+ * @author Dasha Sinkova miss.sinkova-dasha@yandex.ru
+ * @version $id$
+ * @since 0.1
+ */
+package ru.job4j.calculator;
