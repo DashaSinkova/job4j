1f0e5a4f4600347c50e69a7e27c1bacddfce7715
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-18 18:31:45 +0300

    5.7. Ипотека.[#188180]

---
+++ chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class MortgageTest {
+    Mortgage m = new Mortgage();
+    @Test
+    public void when1year(){
+        assertThat(m.year(1000, 1200, 1),is(1));
+
+    }
+    @Test
+    public void when2year(){
+        assertThat(m.year(100, 120, 50),is(2));
+    }
+
+
+}
