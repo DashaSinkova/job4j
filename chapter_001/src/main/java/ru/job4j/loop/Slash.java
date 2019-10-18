313ff58b826864322b266589d75b744a6162b7cc
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-18 20:54:12 +0300

    5.9. Крест в псевдографике. [#188182]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int raw = 0; raw < size; raw++) {
+            for (int cell = 0; cell < size; cell++) {
+                if (raw == cell) {
+                    System.out.print("0");
+                } else if ((raw + cell) % (size - 1) == 0) {
+                    System.out.print("0");
+                } else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3:");
+        draw(3);
+        System.out.println();
+        System.out.println("Draw by 5:");
+        draw(5);
+
+    }
+}
