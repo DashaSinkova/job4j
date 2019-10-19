bd0e7fc6acb3f220b264ee1c1ef1ec1eb0f53e3c
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-19 13:39:08 +0300

    5.2. Создать программу, вычисляющую факториал.[#188174]Исправленный

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -2,7 +2,6 @@ package ru.job4j.loop;
 
 public class Factorial {
     public int calc(int n) {
-        if (n == 0) return 1;
         int res = 1;
         for (int i = 1; i <= n; i++)
             res *= i;
