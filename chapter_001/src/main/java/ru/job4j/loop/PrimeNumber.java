4a400ea9780355639b6943bcf1c31b12dda9e95a
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-20 20:26:05 +0300

    5.6. Простые числа.[#188179]. Исправленный(2)

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -2,19 +2,24 @@ package ru.job4j.loop;
 
 public class PrimeNumber {
     int count = 0;
-    int cheat = 0;
 
     public int calc(int finish) {
 
-        if (finish < 2) return  1;
-        for (int var = 2; var < finish; var++) {
-            if (finish % var == 0) {
+        for (int var = 2; var <= finish; var++)
+            count = check(var);
+
+        return count;
+    }
+
+    public int check(int var) {
+
+        for (int i = 2; i < var; i++) {
+            if (var % i == 0) {
                 count--;
                 break;
             }
         }
-        cheat++;
-        calc(finish - 1);
-        return cheat + count;
+        count++;
+        return count;
     }
 }
