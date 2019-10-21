1725d410cfd48f13c7bda0e8dfb21a0495929a6c
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-21 17:40:06 +0300

    5.5. Простое число.[#188181] Исправленный

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,9 +4,10 @@ public class CheckPrimeNumber {
     public boolean check(int finish) {
         boolean is_simple = true;
         for (int i = 2; i < finish; i++) {
-            if (finish % i == 0)
+            if (finish % i == 0) {
                 is_simple = false;
-            break;
+                break;
+            }
         }
         return is_simple;
     }
