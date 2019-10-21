eae299a8b14fcb022d32514563c6e8ec76c29d90
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-22 00:15:47 +0300

    6.0. Заполнить массив степенями чисел.[#188184]

---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SquareTest {
+    Square sq = new Square();
+    @Test
+    public void whenBound3Then149(){
+        assertThat(sq.calculate(3),is(new int []{1,4,9}));
+    }
+    @Test
+    public void whenBound4Then14916(){
+        assertThat(sq.calculate(4),is(new int []{1,4,9,16}));
+    }
+    @Test
+    public void whenBound6Then149162536(){
+        assertThat(sq.calculate(6),is(new int []{1,4,9,16,25,36}));
+    }
+}
