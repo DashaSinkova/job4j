72b55c90b851515d44cef5e3825f735758becad2
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-22 02:10:09 +0300

    6.4. Слово начинается с ...[#188187]

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+       assertThat(ArrayChar.startsWith(new char[]{'H', 'e', 'l', 'l', 'o'},
+               new char[] {'H', 'e'}), is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        assertThat(ArrayChar.startsWith(new char[]{'H', 'e', 'l', 'l', 'o'},
+                new char[]{'H', 'i'}),is(false));
+
+    }
+}
