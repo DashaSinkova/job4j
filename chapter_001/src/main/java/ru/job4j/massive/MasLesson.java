5c3fa44a791f2c85d38ccdb1ef37bdadb51ae38a
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-21 17:26:44 +0300

    6. Видео.[#188192] Создание нерегулярного двумерного массива и его вывод

--- chapter_001/src/main/java/ru/job4j/massive/MasLesson.java
+++ chapter_001/src/main/java/ru/job4j/massive/MasLesson.java
@@ -1,15 +1,22 @@
 package ru.job4j.massive;
+import java.util.Arrays;
 
 public class MasLesson {
 
     public static void main(String[] args) {
-        int[] mas = new int[5];
-        for (int i = 0; i < mas.length; i++)
-            mas[i] = 10 + i;
-        for (int i = 0; i < mas.length; i++) {
-            mas[i] = mas.length - i;
-            System.out.print(mas[i]+" ");
-
+        int[][] mas = new int[5][];
+        mas[0]= new int[3];
+        mas[1] = new int[5];
+        for (int i = 0; i < 1; i++)
+            for (int j = 0; j < 3; j++){
+                mas [i][j] = i+j*2;
         }
+        for (int i = 1; i<2;i++)
+            for (int j = 0; j<mas[1].length;j++) {
+                mas[i][j] = i + j * 4;
+
+            }
+        System.out.println(Arrays.deepToString(mas));
+
     }
 }
