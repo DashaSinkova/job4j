d80ff34918ab131a8dac9d2e6be6cc7afa36b205
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-22 12:00:02 +0300

    6.5.2. Поиск минимума в диапазоне.[#188199]

---
+++ chapter_001/src/test/java/ru/job4j/array/MinDiapasonTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MinDiapasonTest {
+    @Test
+    public void whenFirstMin(){
+        assertThat(MinDiapason.findMin(new int[]{0,10,2,31,3,2},2,4),is(2));
+    }
+    @Test
+    public void whenSecondMin(){
+        assertThat(MinDiapason.findMin(new int[]{10,1,2,31,3,2},0,2),is(1));
+    }
+    @Test
+    public void whenMinThird(){
+
+        assertThat(MinDiapason.findMin(new int[]{10,1,-2,31,3,2},2,4),is(-2));
+    }
+}
