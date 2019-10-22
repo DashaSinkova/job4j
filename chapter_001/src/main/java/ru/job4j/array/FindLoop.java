aa65661c28795cd10d59fe4a3f0115ffada98752
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-10-22 03:16:40 +0300

    6.5. Поиск индекса в диапазоне.[#188196]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,7 +1,7 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el) {
         int rst = -1;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
@@ -11,5 +11,15 @@ public class FindLoop {
         }
         return rst;
 
+    }
+    public static int indexOf(int[] data, int el, int start, int finish){
+        int rst = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -9,10 +9,12 @@ public class FindLoopTest {
     public void whenArrayHas5Then0(){
         assertThat(fl.indexOf(new int[]{5,3,10},5),is(0));
     }
+    @Test
     public void whenArrayHas11Then1(){
         assertThat(fl.indexOf(new int[]{5,3,10},11),is(-1));
     }
+    @Test
     public void whenArrayHas10Then2(){
-        assertThat(fl.indexOf(new int[]{5,3,10},10),is(2));
+        assertThat(fl.indexOf(new int[]{5,3,3,4,2,1,10,2,3},10,4,7),is(6));
     }
 }
