88c5fafc2640d56b90b2e903f99c389a6556f188
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-11-01 14:15:00 +0300

    1.4. Вызов метод с возвращаемым типом.[#188318]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -4,5 +4,10 @@ public class Cat {
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        System.out.println(peppy.sound());
+    }
+
+    public String sound() {
+        return "may-may";
     }
 }
