71efcb31ecb6837e848b38f3aad7dc8a5f2138e1
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2019-11-06 13:52:11 +0300

    3. Перегрузить метод max для трех чисел[#188311]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -6,5 +6,8 @@ public class PointTest {
     public void distance() {
         Assert.assertEquals(2, new Point(0, 2).distance(new Point(0, 0)), 0.01);
     }
-
+    @Test
+    public void distance3d() {
+        Assert.assertEquals(2, new Point(0, 0, 1).distance3d(new Point(0, 2, 1)), 0.01);
+    }
 }
