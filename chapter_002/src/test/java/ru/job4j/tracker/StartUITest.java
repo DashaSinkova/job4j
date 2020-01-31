3279c2919a4b5956f00e2dfe27c0523389e9299e
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-01-31 18:14:26 +0300

    5.1. Тестирование. Подготовка данных.[#188297]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,5 +14,25 @@ public class StartUITest {
             Item expected = new Item("Fix PC");
             assertThat(created.getName(), is(expected.getName()));
         }
+        @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {item.getId(), "replaced item"};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+        }
+        @Test
+    public void whenDelete() {
+            Tracker tracker = new Tracker();
+            Item item = new Item("new item");
+            tracker.add(item);
+            String[] answers = {item.getId()};
+            StartUI.deteleItem(new StubInput(answers), tracker);
+            Item expected = null;
+            assertThat(tracker.findById(item.getId()), is(expected));
+        }
     }
 
