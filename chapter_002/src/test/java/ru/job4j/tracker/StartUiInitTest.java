95552a474b1485a25957c95e0c678b4ba3a4b01f
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-02-18 18:25:21 +0300

    9. Написать тесты на StartUI.[#188285]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUiInitTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+import org.junit.Test;
+import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.*;
+public class StartUiInitTest {
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(new String[]{"0"});
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        assertThat(action.isCall(), is(true));
+    }
+}
