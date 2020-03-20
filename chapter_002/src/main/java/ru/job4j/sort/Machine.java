2a126fd43765394a9214780bb1f21f555b58e398
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-03-21 01:52:58 +0300

    Сдача в кофе машине. [#34741]

---
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,29 @@
+package ru.job4j.sort;
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int change = money - price;
+        int size = 0;
+        for (int i = 0, j = 0; i < COINS.length; i++) {
+            boolean flag = false;
+            while (change > 1) {
+                int res = change - COINS[i];
+                if (res < 0) {
+                    break;
+                }
+                change = res;
+                flag = true;
+            }
+            if (flag) {
+                rsl[j++] = COINS[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
+
