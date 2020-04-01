78307792d64aecf6018de4edc8bda087ce21d309
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-04-01 12:29:47 +0300

    3. Конвертация ArrayList в двухмерный массив[#188259]

---
+++ chapter_003/src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertList2ArrayTest {
+    @Test
+    public void whenConvertToMas() {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7);
+        int[][] expected = {{1, 2, 3},{4, 5, 6},{7, 0, 0}};
+        assertThat(ConvertList2Array.toArray(list, 3), is(expected));
+    }
+}
