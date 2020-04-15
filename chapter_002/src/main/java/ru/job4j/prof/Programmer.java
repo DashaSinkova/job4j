ca1bf196a315bad9654033bc021951733cb80b8a
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-04-15 18:52:38 +0300

    add 2 files

--- chapter_002/src/main/java/ru/job4j/prof/Programmer.java
+++ chapter_002/src/main/java/ru/job4j/prof/Programmer.java
@@ -1,17 +1,17 @@
-//package ru.job4j.prof;
-///**
-// * Курс Стажер 002ООП.2.3
-// */
-//public class Programmer extends Engineer {
-//    private String levelOfExpertise;
-//    public Programmer(String name, String surname, String education, String birthday, String clothes, String tools, String placeOfWork, String levelOfExpertise) {
-//        super(name, surname, education, birthday, clothes, tools, placeOfWork);
-//        this.levelOfExpertise = levelOfExpertise;
-//    }
-//    public String getLevelOfExpertise() {
-//        return levelOfExpertise;
-//    }
-//    public void setLevelOfExpertise(String levelOfExpertise) {
-//        this.levelOfExpertise = levelOfExpertise;
-//    }
-//}
+package ru.job4j.prof;
+/**
+ * Курс Стажер 002ООП.2.3
+ */
+public class Programmer extends Engineer {
+    private String levelOfExpertise;
+    public Programmer(String name, String surname, String education, String birthday, String clothes, String tools, String placeOfWork, String levelOfExpertise) {
+        super(name, surname, education, birthday, clothes, tools, placeOfWork);
+        this.levelOfExpertise = levelOfExpertise;
+    }
+    public String getLevelOfExpertise() {
+        return levelOfExpertise;
+    }
+    public void setLevelOfExpertise(String levelOfExpertise) {
+        this.levelOfExpertise = levelOfExpertise;
+    }
+}
