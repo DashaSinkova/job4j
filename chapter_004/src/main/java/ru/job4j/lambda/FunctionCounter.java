95cb5e7736bf7cf28ab048f8cda053358d5340b7
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-04-29 11:11:54 +0300

    1. Подсчет функции в диапазоне.[#188128]

--- chapter_004/src/main/java/ru/job4j/lambda/FunctionCounter.java
+++ chapter_004/src/main/java/ru/job4j/lambda/FunctionCounter.java
@@ -7,10 +7,8 @@ import java.util.function.Function;
 public class FunctionCounter {
    public List<Double> diapason(int start, int end, Function<Double, Double> func) {
        List<Double> res = new ArrayList<>();
-       while (start < end) {
-           double funcRes = func.apply((double) start);
-           res.add(funcRes);
-           start++;
+       for (double value = start; value < end; value++) {
+           res.add(func.apply(value));
        }
        return res;
    }
