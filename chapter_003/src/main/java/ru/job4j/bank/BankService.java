a308a71c7ab6fed23035c642e245f187e664cee7
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2020-05-26 14:25:19 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#188134]

--- chapter_003/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_003/src/main/java/ru/job4j/bank/BankService.java
@@ -1,6 +1,7 @@
 package ru.job4j.bank;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
@@ -11,31 +12,25 @@ public class BankService {
         User user = findByPassport(passport);
         if (user != null) {
             List<Account> accounts = users.get(user);
-            if (!accounts.contains(account)) {
+            if (accounts.stream().noneMatch(el -> el.equals(account))) {
                 accounts.add(account);
             }
         }
     }
     public User findByPassport(String passport) {
         User res = null;
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                res = user;
-                break;
+            Optional<User> user = users.entrySet().stream().map(Map.Entry::getKey).filter(el -> el.getPassport().equals(passport)).findAny();
+            if (user.isPresent()) {
+                res = user.get();
             }
-        }
             return res;
     }
     public Account findByRequisite(String passport, String requisites) {
         Account res = null;
         User user = findByPassport(passport);
         if (user != null) {
-          for (Account account : users.get(user)) {
-              if (account.getRequisite().equals(requisites)) {
-                  res = account;
-                  break;
-              }
-            }
+//            res = users.entrySet().stream().map(Map.Entry::getValue).flatMap(Collection::stream).filter(el -> el.getRequisite().equals(requisites)).findAny().get();
+            res = users.entrySet().stream().filter(el -> el.getKey().equals(user)).map(Map.Entry::getValue).flatMap(Collection::stream).filter(el -> el.getRequisite().equals(requisites)).findAny().get();
         }
         return res;
     }
