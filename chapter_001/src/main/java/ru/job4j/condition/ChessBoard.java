d3a4a44adf84d24a67929270c34cba9bba23d011
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2021-06-25 19:26:05 +0300

    4.3.6. Math.abs [#373616 #171182]

--- chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
+++ chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
@@ -4,12 +4,7 @@ import java.util.Objects;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-        try {
-            Objects.checkIndex(x1, 8);
-            Objects.checkIndex(x2, 8);
-            Objects.checkIndex(y2, 8);
-            Objects.checkIndex(y1, 8);
-        } catch (Exception e) {
+        if ((x1 < 0 || x1 > 7) || (x2 < 0 || x2 > 7) || (y1 < 0 || y1 > 7) || (y2 < 0 || y2 > 7)) {
             return 0;
         }
         return Math.abs(x2 - x1) == Math.abs(y2 - y1) ? Math.abs(y2 - y1) : 0;
