29433636843e690ca37ce3f0d69255748e481b4f
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2021-06-29 16:44:39 +0300

    6.0.5. Массивы в тестах. [#226789]

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -1,11 +1,18 @@
 package ru.job4j.array;
 
 public class Square {
-    public int[] calculate(int bound) {
+    public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int i = 0; i < bound; i++) {
-            rst[i] = (i + 1) * (i + 1);
+            rst[i] = i * i;
         }
         return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] array = calculate(4);
+        for (int el : array) {
+            System.out.println(el);
+        }
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -1,20 +1,22 @@
 package ru.job4j.array;
 import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class SquareTest {
     Square sq = new Square();
     @Test
-    public void whenBound3Then149() {
-        assertThat(sq.calculate(3), is(new int[]{1, 4, 9}));
+    public void whenBound3Then014() {
+        assertArrayEquals(sq.calculate(3), new int[]{0, 1, 4});
     }
     @Test
-    public void whenBound4Then14916() {
-        assertThat(sq.calculate(4), is(new int[]{1, 4, 9, 16}));
+    public void whenBound4Then014916() {
+        assertThat(sq.calculate(5), is(new int[]{0, 1, 4, 9, 16}));
     }
     @Test
-    public void whenBound6Then149162536() {
-        assertThat(sq.calculate(6), is(new int[]{1, 4, 9, 16, 25, 36}));
+    public void whenBound6Then0149162536() {
+        assertThat(sq.calculate(7), is(new int[]{0, 1, 4, 9, 16, 25, 36}));
     }
 }
