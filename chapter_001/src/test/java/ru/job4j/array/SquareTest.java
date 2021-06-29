9309b5b54d7d3af526013bacb9958080383d3cf3
Dasha Sinkova <miss.sinkova-dasha@yandex.ru>
2021-06-29 21:47:49 +0300

    6.0.5. Массивы в тестах. [#226789 #172726]

--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -6,17 +6,19 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class SquareTest {
-    Square sq = new Square();
     @Test
     public void whenBound3Then014() {
+        Square sq = new Square();
         assertArrayEquals(sq.calculate(3), new int[]{0, 1, 4});
     }
     @Test
     public void whenBound4Then014916() {
+        Square sq = new Square();
         assertThat(sq.calculate(5), is(new int[]{0, 1, 4, 9, 16}));
     }
     @Test
     public void whenBound6Then0149162536() {
+        Square sq = new Square();
         assertThat(sq.calculate(7), is(new int[]{0, 1, 4, 9, 16, 25, 36}));
     }
 }
